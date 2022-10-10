7cc109a0fe3348d8ddf04b15b8a11e4e242a80a4
juliocesar014 <juliocesar014@live.com>
2022-10-10 08:43:42 -0300

    add for/faker/view

--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from utils.receitas.factory import make_receita
+
+
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -7,11 +10,12 @@ def home(request):
     # Return HTTP Response -> Escrever ('html')
     # Return Render -> Renderizar template Html
     return render(request, 'receitas/pages/home.html', context={
-        'name': 'Júlio',
-        'idade': '20',
+        'receitas': [make_receita() for _ in range(10)]
 
     })
 
 
 def receita(request, id):
-    return render(request, 'receitas/pages/receita-view.html')
+    return render(request, 'receitas/pages/receita-view.html', context={
+        'receita': make_receita(),
+    })
